4df7fc07e4d2c86cbff364c4e18855da7a5ca86b
Prasannachowdula <prasanna.chowdula798@gmail.com>
2025-04-05 12:31:01 +0530

    changes

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import event from "../assets/events.png"
 import food7 from "../assets/food7.png"
+
 const Events = () => {
   return (
-    <div className='w-full h-[550px] relative flex items-center bg-cover bg-center bg-no-repeat'
-    style={{backgroundImage:`url(${event})`}}>
-         <div className='w-full font-poppins flex flex-col gap-1 text-white p-20 px-32 h-[60%] bg-black'>
-              <h1 className='font-semibold text-red-500'>discount</h1>
-              <h1 className='text-4xl font-bold'>Upcoming Events</h1>
-              <p className='w-4/12 mt-4 text-neutral-400'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</p>
-         </div>
-         <div className='w-full h-full absolute p-10 pr-20 flex justify-end'>
-            <img src={food7} className=''/>
-         </div>
+    <div
+      className='w-full h-auto relative flex flex-col lg:flex-row items-center bg-cover bg-center bg-no-repeat'
+      style={{ backgroundImage: `url(${event})` }}
+    >
+      <div className='w-full font-poppins flex flex-col gap-1 text-white p-6 sm:p-10 lg:p-20 lg:px-32 h-auto lg:h-[60%] bg-black bg-opacity-70'>
+        <h1 className='font-semibold text-red-500 text-sm sm:text-base'>discount</h1>
+        <h1 className='text-2xl sm:text-3xl lg:text-4xl font-bold'>Upcoming Events</h1>
+        <p className='mt-4 text-sm sm:text-base text-neutral-400 max-w-full lg:w-4/12'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+        </p>
+      </div>
+
+      <div className='w-full h-full absolute lg:static top-0 left-0 p-4 sm:p-10 lg:pr-20 flex justify-center lg:justify-end items-end'>
+        <img src={food7} className='w-3/4 sm:w-2/3 lg:w-auto object-contain' />
+      </div>
     </div>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
 import logo from "../assets/logo1.png"
-import { IoLogoFacebook } from "react-icons/io5";
-import { RiInstagramFill } from "react-icons/ri";
+import { IoLogoFacebook } from "react-icons/io5"
+import { RiInstagramFill } from "react-icons/ri"
+
 const Footer = () => {
   return (
-    <div className='w-full flex flex-col font-instrument-sans text-white font px-32 py-10 h-[350px] bg-black'>
-        <div className='border-b flex justify-between items-center border-b-white '>
-           <div className='flex flex-col gap-2 items-center pb-10'><img src={logo} className='h-30 w-40 object-center '/>
-           <p>Phone number</p>
-           <p>+34 911 72 07 45</p>
-           </div>
-           <ul className='flex text-lg pr-4 gap-5'>
-            <li>Home</li>
-            <li>Menu</li>
-            <li>Wines</li>
-            <li>Christmas Dinner</li>
-           </ul>
+    <div className='w-full flex flex-col font-instrument-sans text-white px-6 lg:px-32 py-10 bg-black'>
+      <div className='border-b border-b-white flex flex-col lg:flex-row justify-between items-start lg:items-center pb-10 gap-6'>
+        <div className='flex flex-col gap-2 items-start lg:items-center'>
+          <img src={logo} className='h-20 w-40 object-center' />
+          <p>Phone number</p>
+          <p>+34 911 72 07 45</p>
         </div>
-        <div className='flex justify-between items-center py-10'>
-        <p>copyright &copy; 2025 Brastra</p>
-        <div className='flex gap-3 text-4xl'>
-            <IoLogoFacebook/>
-            <RiInstagramFill/>
+        <ul className='flex flex-col sm:flex-row text-base sm:text-lg gap-2 sm:gap-5'>
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Wines</li>
+          <li>Christmas Dinner</li>
+        </ul>
+      </div>
+      <div className='flex flex-col sm:flex-row justify-between items-start sm:items-center pt-6 gap-4'>
+        <p className='text-sm sm:text-base'>copyright &copy; 2025 Brastra</p>
+        <div className='flex gap-3 text-2xl sm:text-4xl'>
+          <IoLogoFacebook />
+          <RiInstagramFill />
         </div>
-        </div>
+      </div>
     </div>
   )
 }
